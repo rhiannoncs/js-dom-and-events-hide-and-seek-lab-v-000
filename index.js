e4ec6081553a5fa7869745e62fc3cf6cc2392cad
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-06-10 21:41:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  let rankedLists = document.querySelectorAll('.ranked-list li');
+  
+  rankedLists.forEach(function(element) {
+    let integer = parseInt(element.innerHTML);
+    element.innerHTML = (integer += n).toString();
+  })
+  
+  return rankedLists;
+}
+
+function deepestChild() {
+  const grandNode = document.querySelector("#grand-node");
+  
+  
+}
